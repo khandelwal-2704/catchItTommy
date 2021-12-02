5824a780f32c3a0143e982ca9bb3ef906eedca71
Anvesh-Som <anvesh.s.som@gmail.com>
2021-12-03 02:36:29 +0530

    removed extra lines

--- hardware/electronicsDesign/sketches/pySerial_Receiver/pySerial_Receiver.ino
+++ hardware/electronicsDesign/sketches/pySerial_Receiver/pySerial_Receiver.ino
@@ -10,18 +10,11 @@ float floatFromPC = 0.0;
 
 boolean newData = false;
 
-//============
-
 void setup() {
     Serial.begin(115200);
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, LOW);
-//    Serial.println("This demo expects 3 pieces of data - text, an integer and a floating point value");
-//    Serial.println("Enter data in this style <HelloWorld, 12, 24.7>  ");
-//    Serial.println();
 }
-
-//============
 
 void loop() {
     recvWithStartEndMarkers();
@@ -34,8 +27,6 @@ void loop() {
         newData = false;
     }
 }
-
-//============
 
 void recvWithStartEndMarkers() {
     static boolean recvInProgress = false;
@@ -69,35 +60,20 @@ void recvWithStartEndMarkers() {
     }
 }
 
-//============
-
 void parseData() {      // split the data into its parts
 
     char * strtokIndx; // this is used by strtok() as an index
 
     strtokIndx = strtok(tempChars,",");      // get the first part - the string
- //   strcpy(messageFromPC, strtokIndx); // copy it to messageFromPC
     xFromPC = atoi(strtokIndx);
  
     strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
     yFromPC = atoi(strtokIndx);     // convert this part to an integer
-
-//    strtokIndx = strtok(NULL, ",");
-  //  floatFromPC = atof(strtokIndx);     // convert this part to a float
-
 }
 
-//============
-
 void showParsedData() {
-//    Serial.print("Message ");
-//    Serial.println(messageFromPC);
-//    Serial.print("xInteger ");
-//    Serial.println(xFromPC);
     if(yFromPC > 240 and xFromPC > 240)
     digitalWrite(LED_BUILTIN, HIGH);
     else if(yFromPC < 240 and xFromPC < 240)
     digitalWrite(LED_BUILTIN, LOW);
-//    Serial.print("yInteger ");
-//    Serial.println(yFromPC);
 }
